f3774ee43e35ceb08beb957d191fb1e92d66319b
congdanh1252 <70893956+congdanh1252@users.noreply.github.com>
2020-11-30 13:14:46 +0700

    Add files via upload

---
+++ ModLessonForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace StudentSupportApp
+{
+    public partial class ModLessonForm : Form
+    {
+        MainForm parent;
+        private string sUserID;
+        private string[] sOldInfo;
+
+        public ModLessonForm()
+        {
+            InitializeComponent();
+        }
+
+        public ModLessonForm(MainForm parent, List<string> data, string ID)
+        {
+            try
+            {
+                InitializeComponent();
+                this.parent = parent;
+                this.sUserID = ID;
+                this.sOldInfo = new string[6];
+                if (data.Count > 4)
+                {
+                    tbxSemNaML.Text = data[0];
+                    cbxDIWML.Text = data[1];
+                    tbxSubNaML.Text = data[2];
+                    tbxSubIDML.Text = data[3];
+                    cbxTimeOrderM.Text = data[4];
+
+                    for (int i = 0; i < data.Count; i++)
+                    {
+                        this.sOldInfo[i] = data[i];
+                    }
+                    this.sOldInfo[5] = this.sUserID;
+                }
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+        }
+
+        private void btnExitML_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.parent.Show();
+        }
+
+        private void btnSaveML_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cbxDIWML.Text != "" && cbxTimeOrderM.Text != "" && tbxSemNaML.Text != "" && tbxSubIDML.Text != "" && tbxSubNaML.Text != "")
+                {
+                    Timetable lesson = new Timetable();
+                    string[] sData = new string[] { cbxDIWML.Text, cbxTimeOrderM.Text, tbxSubIDML.Text.Remove(0, 1), tbxSubNaML.Text, tbxSemNaML.Text, this.sUserID };
+                    if (lesson.UpdateLessonInfo(sOldInfo, sData) == 1)
+                        MessageBox.Show("Sucessfully modified!", "Modify Lesson Information");
+                    else MessageBox.Show("Failed to modify lesson. Check your data.", "Modify Lesson");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to modify lesson. Check your data.", "Modify Lesson");
+                }
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+        }
+
+        private void tbxSemNaML_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                cbxDIWML.Focus();
+        }
+
+        private void cbxDIWML_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                tbxSubNaML.Focus();
+        }
+
+        private void tbxSubNaML_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                tbxSubIDML.Focus();
+        }
+
+        private void tbxSubIDML_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                cbxTimeOrderM.Focus();
+        }
+
+        private void cbxTimeOrderM_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnSaveML_Click(sender, e);
+        }
+    }
+}
---
+++ Timetable.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace StudentSupportApp
+{
+    class Timetable
+    {
+        USER user = new USER();
+        private string sSemester;
+        List<string> sLesson = new List<string> { };
+
+        public Timetable(string ID, string Semester)
+        {
+            user.ID = ID;
+            this.sSemester = Semester;
+        }
+
+        public Timetable()
+        {
+            sSemester = "1";
+        }
+
+        public string Semester
+        {
+            get { return this.sSemester; }
+            set { this.sSemester = value; }
+        }
+
+        public void LoadUserTimetable()
+        {
+            try
+            {
+                Connect loadTimetable = new Connect();
+                loadTimetable.OpenConnection();
+
+                string[] sWeekDay = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+                for (int j = 0; j < 7; j++)
+                {
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        string sLoadData = "select SUB_NAME, SUB_ID from LESSON where ID_USER='" + this.user.ID
+                                         + "' AND DAYINWEEK='" + sWeekDay[j] + "' AND SEM_NAME='"
+                                         + this.sSemester + "' AND TIMEORDER=" + i;
+                        SqlCommand loadDay = loadTimetable.CreateSQLCmd(sLoadData);
+                        SqlDataReader reader = loadDay.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            if (reader.Read() == false)
+                            {
+                                break;
+                            }
+                            sLesson.Add(reader.GetString(0) + "\n" + reader.GetString(1));
+                        }
+                        else sLesson.Add("");
+                        reader.Close();
+                    }
+                }
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+        }
+
+        public void LoadTimetableToDGV(DataGridView obj)
+        {
+            try
+            {
+                int iLessonIndex = 0;
+                for (int j = 1; j <= 7; j++)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        obj.Rows[i].Cells[j].Value = sLesson[iLessonIndex];
+                        iLessonIndex++;
+                    }
+                }
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+        }
+
+        public int AddLessonToDatabase(string[] args)
+        {
+            Connect AddLesson = new Connect();
+            try
+            {
+                AddLesson.OpenConnection();
+                string sAddData = "INSERT INTO LESSON VALUES('" + args[0] + "', " + args[1] + ", '" + args[2]
+                                                           + "', '" + args[3] + "', '" + args[4] + "', '" + args[5] + "')";
+                if (CheckExistSemester(args[4]) == 0)
+                {
+                    string sAddSem = "INSERT INTO SEMESTER VALUES('" + args[5] + "', '" + args[4] + "')";
+                    SqlCommand AddCommand2 = AddLesson.CreateSQLCmd(sAddSem);
+                    AddCommand2.ExecuteNonQuery();
+                }
+                SqlCommand AddCommand1 = AddLesson.CreateSQLCmd(sAddData);
+                AddCommand1.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problem with connecting to server or with data adding.");
+                return 0;
+            }
+            AddLesson.CloseConnection();
+            return 1;
+        }
+
+        public int UpdateLessonInfo(string[] oldInfo, string[] newInfo)
+        {
+            Connect ModLesson = new Connect();
+            try
+            {
+                ModLesson.OpenConnection();
+                string sUpdateData = "UPDATE LESSON " +
+                    "              SET DAYINWEEK='" + newInfo[0] + "' , TIMEORDER=" + newInfo[1] +
+                                        " , SUB_ID='" + newInfo[2] + "' , SUB_NAME='" + newInfo[3] +
+                                        "' , SEM_NAME='" + newInfo[4] +
+                                   "' WHERE ID_USER='" + oldInfo[5] + "' AND DAYINWEEK='" + oldInfo[1] +
+                                                    "' AND TIMEORDER=" + oldInfo[4] + " AND SUB_ID='" + oldInfo[3].Remove(0, 1) +
+                                                    "' AND SUB_NAME='" + oldInfo[2] + "' AND SEM_NAME='" + oldInfo[0] + "'";
+
+                SqlCommand UpdateCommand = ModLesson.CreateSQLCmd(sUpdateData);
+                UpdateCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problem with connecting to server or with data updating.");
+                return 0;
+            }
+            ModLesson.CloseConnection();
+            return 1;
+        }
+
+        public int CheckExistSemester(string input)
+        {
+            try
+            {
+                Connect Check = new Connect();
+                Check.OpenConnection();
+
+                SqlCommand command = Check.CreateSQLCmd("select distinct SEM_NAME from SEMESTER where ID='" + this.user.ID + "'");
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.HasRows)
+                {
+                    if (reader.Read() == false) break;
+                    if (reader.GetString(0) == input) return 1;
+                }
+                return 0;
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+            return 0;
+        }
+    }
+}
